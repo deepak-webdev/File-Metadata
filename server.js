b32a9c4277ad55a1188bb2c0c6e34780615809b9
Glitch (heavenly-gentle-camp) <none>
2020-09-21 18:42:11 +0000

    🚒👀 Updated with Glitch

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
+var multer = require('multer')
+var upload = multer({dest:'uploads/'})
 // require and use "multer"...
 
 var app = express();
@@ -14,8 +15,13 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse',upload.single('upfile'), function(req, res){
+         const {originalname:name,mimetype:type,size} = req.file;
+res.json({
+  name,
+  type,
+  size
+})
 });
 
 app.listen(process.env.PORT || 3000, function () {
